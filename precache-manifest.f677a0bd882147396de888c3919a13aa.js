dcd1c964cc49c2a586bf67aea8bd1fa60ff09089
emailijic <emailijic@elfak.rs>
2020-10-14 22:23:40 +0200

    Updates

---
+++ precache-manifest.f677a0bd882147396de888c3919a13aa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10348febea3570ebc7c9a196ce3e5a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db1ef1ea18d6850a328c",
+    "url": "/portfolio/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "571fcb29a0c1a599cf26",
+    "url": "/portfolio/static/css/main.15dadd8f.chunk.css"
+  },
+  {
+    "revision": "db1ef1ea18d6850a328c",
+    "url": "/portfolio/static/js/2.8727ea22.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/portfolio/static/js/2.8727ea22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571fcb29a0c1a599cf26",
+    "url": "/portfolio/static/js/main.116180bf.chunk.js"
+  },
+  {
+    "revision": "af5e4d1703167f3b0fee",
+    "url": "/portfolio/static/js/runtime-main.174e15cc.js"
+  },
+  {
+    "revision": "b247346e69f2278272f06bddde5c2c98",
+    "url": "/portfolio/static/media/anothersample.b247346e.jpg"
+  },
+  {
+    "revision": "e4cd8a253229c567ad6fd110bcfc12a9",
+    "url": "/portfolio/static/media/cargo-ship.e4cd8a25.svg"
+  },
+  {
+    "revision": "e958bf9662ba1529312c70e1ee5e9006",
+    "url": "/portfolio/static/media/food-and-restaurant.e958bf96.svg"
+  },
+  {
+    "revision": "1b2585ec3167e2870ff9c85638dcf964",
+    "url": "/portfolio/static/media/garment.1b2585ec.svg"
+  },
+  {
+    "revision": "8f3c24fcf5d00466c811efa0654fda6b",
+    "url": "/portfolio/static/media/jobify (2).8f3c24fc.png"
+  },
+  {
+    "revision": "910ba6aff30fda831b44542882c76889",
+    "url": "/portfolio/static/media/left-arrow.910ba6af.svg"
+  },
+  {
+    "revision": "dabfea89de92aa5c697dc22dcd4e6f08",
+    "url": "/portfolio/static/media/onechat.dabfea89.png"
+  },
+  {
+    "revision": "51f8512bd189c16e9d29919160862b8a",
+    "url": "/portfolio/static/media/profile-picture.51f8512b.jpg"
+  },
+  {
+    "revision": "b4c3239241b92d922429ddc70121095a",
+    "url": "/portfolio/static/media/ready-vet.b4c32392.png"
+  },
+  {
+    "revision": "65516f1749a6b6975311c09df711aae8",
+    "url": "/portfolio/static/media/right-arrow.65516f17.svg"
+  }
+]);
